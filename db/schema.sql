ee22c7755bbd2d42b70e5322e2dfffcfd00981cf
Jake Garlick <jacobgarlick@msn.com>
2019-10-16 17:16:27 -0600

    built out schema and seed data for SQL

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers 
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Southwest", false)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar", false)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean", false)
