3a0ee46fad30e7cd93e370f5e315684913824c22
Jake Garlick <jacobgarlick@msn.com>
2019-10-17 17:22:24 -0600

    tied into bootstrap for styling, updated seed data for more values to begin with

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Southwest", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean", true)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie", true)
