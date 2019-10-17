02bac9535a1a38ed21bbde44cee977b4aca05bad
Jake Garlick <jacobgarlick@msn.com>
2019-10-17 10:01:16 -0600

    updated seeds data, updated handlebars, added new partials view for handlebars to show if burger is consumed or not

---
+++ public/assets/js/burgerjs.js
@@ -0,0 +1,44 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEat = $(this).data("neweat");
+  
+      var newEatState = {
+        devoured: newEat
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatState
+      }).then(
+        function() {
+          console.log("changed devoured to", newEat);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newCat = {
+        name: $("#burgs").val().trim(),
+        sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
