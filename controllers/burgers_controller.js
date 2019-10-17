256bbe5c4eca81b75ce20be88ae4cbd801f93e3b
Jake Garlick <jacobgarlick@msn.com>
2019-10-16 19:20:54 -0600

    built out burger controller javascript to plug in with rest of MVC

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res){
+    burger.selectAll(function(data){
+        var moreBurger = {
+            burgers: data
+        };
+        console.log(moreBurger);
+        res.render("index", moreBurger)
+    });
+});
+
+router.post("/api/burgers", function (req, res){
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (data){
+        res.json({ id: data.insertId })
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    var idHold = "id = " + req.params.id;
+    console.log("ID Hold", idHold);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, idHold, function (data){
+        if (data.changedRows == 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
