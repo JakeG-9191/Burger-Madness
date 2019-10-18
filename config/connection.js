00913fd07a6195a84dd0168005512025727e0ca3
Jake Garlick <jacobgarlick@msn.com>
2019-10-17 20:04:58 -0600

    changes to connection and git ignore files

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ if (process.env.JAWSDB_URL) {
 } else {
  connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "deadeye22",
     database: "burgers_db"
